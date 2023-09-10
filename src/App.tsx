8eb7563ed28baad9cd5b2435e00d9ea97fd64c5d
Inhu Jo <inhudev@gmail.com>
2023-09-10 18:40:12 +0900

    refactor : TodoPage에서 카테고리 초기화하도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from './styles/GlobalStyles';
 import theme from './styles/theme';
 import { TodoPage } from './pages/TodoPage';
-import { initCategoryList } from './utils/category';
 
 export default function App() {
-  /**
-   * 첫 로딩할 때 스토리지에 초기화
-   */
-  useEffect(() => {
-    initCategoryList();
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+
+import { initCategoryList } from '@/utils/category';
+
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { Sidebar } from '@/components/Sidebar';
@@ -6,6 +10,10 @@ import { CategoryProvider } from '@/context/provider';
 import { Container, Wrapper } from './TodoPage.style';
 
 export const TodoPage = () => {
+  useEffect(() => {
+    initCategoryList();
+  }, []);
+
   return (
     <Container>
       <Header />
