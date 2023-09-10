991f05f354797c6fd40e41e5222398ae97ed6756
Inhu Jo <inhudev@gmail.com>
2023-09-10 17:36:36 +0900

    feat : HTML 타이틀 변경

--- webpack.common.js
+++ webpack.common.js
@@ -30,6 +30,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      title: 'DAILY LOG',
     }),
     new ProvidePlugin({
       React: 'react',
