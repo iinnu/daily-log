d85d0dd5bcfda8a9e90a097beccc2ac7cb2a97ff
Inhu Jo <inhudev@gmail.com>
2023-09-10 22:37:51 +0900

    feat : 메모에 개행문자 포함해서 출력

--- src/components/Sidebar/Memo/styles.tsx
+++ src/components/Sidebar/Memo/styles.tsx
@@ -3,12 +3,14 @@ import { styled } from 'styled-components';
 export const CommentBox = styled.div`
   padding: 20px;
   line-height: 1.5;
+  white-space: pre;
 `;
 
 export const TextArea = styled.textarea`
   padding: 20px;
   line-height: 1.5;
   width: 100%;
+  resize: none;
 `;
 
 export const ButtonArea = styled.div`
